9fa61ca6b23424616cfb0745ab7ce5328cfbd96c
RedstoneParadox <spinjitzu00@gmail.com>
2019-09-05 18:20:08 -0700

    removed ToolPart class

--- src/main/java/redstoneparadox/expandedtoolcraft/parts/ToolPart.kt
+++
@@ -1,9 +0,0 @@
-package redstoneparadox.expandedtoolcraft.parts
-
-import net.minecraft.util.Identifier
-import redstoneparadox.expandedtoolcraft.item.PartItem
-
-class ToolPart(val name : String) {
-
-
-}
